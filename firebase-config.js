c5120f087ee34782a060122b1f3f6c7b56297072
Daniel <87232170+abacaxin@users.noreply.github.com>
2026-07-15 01:28:04 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,10 @@
  * Consulte FIREBASE_SETUP.md para o passo a passo completo.
  */
 window.CNM_FIREBASE_CONFIG = {
-  apiKey: 'SUBSTITUA_PELA_SUA_API_KEY',
-  authDomain: 'SUBSTITUA.firebaseapp.com',
-  projectId: 'SUBSTITUA',
-  storageBucket: 'SUBSTITUA.firebasestorage.app',
-  messagingSenderId: 'SUBSTITUA',
-  appId: 'SUBSTITUA'
+  apiKey: 'AIzaSyD2C-kAepi01o3SC51afnHzqQ_4b3FlnaM',
+  authDomain: 'cnmbr-fa7b3.firebaseapp.com',
+  projectId: 'cnmbr-fa7b3',
+  storageBucket: 'cnmbr-fa7b3.firebasestorage.app',
+  messagingSenderId: '984198981934',
+  appId: '1:984198981934:web:6bf4790755193c627632bf'
 };
